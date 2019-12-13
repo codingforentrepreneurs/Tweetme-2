c118bac532475dc16052c0ce5dce2d264d5c333a
CFE <hello@teamcfe.com>
2019-12-13 15:41:28 -0800

    6 - Updated VS Code Config

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#)=h)_wc*k%f=wk+!$x0t%1wx7*_50$a1%*75s$og(8$27$ju1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
