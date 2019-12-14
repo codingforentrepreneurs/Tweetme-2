f52194e263c6600b43b11dfcb10ad686b155dd92
CFE <hello@teamcfe.com>
2019-12-14 15:05:45 -0800

    15 - Tweet List View

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path # url()
 
-from tweets.views import home_view, tweet_detail_view
+from tweets.views import home_view, tweet_detail_view, tweet_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('tweets', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -7,6 +7,21 @@ from .models import Tweet
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
