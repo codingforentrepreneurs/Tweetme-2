ef2e008e2a2b66130ec6e37a7b349f62ade71607
CFE <hello@teamcfe.com>
2019-12-14 14:38:24 -0800

    10 - Intro to URL Routing and Dynamic Routing

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from tweets.views import home_view, tweet_detail_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view),
+    path('tweets/<int:tweet_id>', tweet_detail_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h1>Hello World</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
