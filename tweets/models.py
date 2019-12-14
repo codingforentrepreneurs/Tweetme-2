c08ef2bb515709610161838223b0b16fc0fc4cf3
CFE <hello@teamcfe.com>
2019-12-13 16:19:27 -0800

    9 - Store Data from Django Model

--- tweets/models.py
+++ tweets/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Tweet(models.Model):
+    # Maps to SQL data
     # id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
