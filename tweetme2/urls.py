9b7d4ce7cdc049de4d77716599a3a9fed3fe7e2d
CFE <hello@teamcfe.com>
2019-12-14 14:42:27 -0800

    11 - Handling Dynamic Routing

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path # url()
 
 from tweets.views import home_view, tweet_detail_view
 
--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+
+from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
     return HttpResponse("<h1>Hello World</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except:
+        raise Http404
+    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
