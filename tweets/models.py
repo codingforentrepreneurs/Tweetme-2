c088d8b548a7c981f32a0cfd7151cff4a11fd528
CFE <hello@teamcfe.com>
2019-12-15 15:12:00 -0800

    30 - Append New Tweet & Reorder

--- tweets/models.py
+++ tweets/models.py
@@ -8,6 +8,9 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
+    class Meta:
+        ordering = ['-id']
+    
     def serialize(self):
         return {
             "id": self.id,
