b553fc641b4e40722aa632caa4aa5bad7e63879f
CFE <hello@teamcfe.com>
2019-12-16 15:53:32 -0800

    35 - Associate Authenticated User to Object

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '#)=h)_wc*k%f=wk+!$x0t%1wx7*_50$a1%*75s$og(8$27$ju1'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.cfe.sh']
-
+LOGIN_URL = "/login"
 
 # Application definition
 
--- tweets/views.py
+++ tweets/views.py
@@ -14,11 +14,18 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_create_view(request, *args, **kwargs):
+    user = request.user
+    if not request.user.is_authenticated:
+        user = None
+        if request.is_ajax():
+            return JsonResponse({}, status=401)
+        return redirect(settings.LOGIN_URL)
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
     if form.is_valid():
         obj = form.save(commit=False)
         # do other form related logic
+        obj.user = user
         obj.save()
         if request.is_ajax():
             return JsonResponse(obj.serialize(), status=201) # 201 == created items
