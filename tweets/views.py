4b764cbc57f533e25b5fa973342da550a1e3ab91
CFE <hello@teamcfe.com>
2019-12-16 14:31:14 -0800

    31 - Handling Form Errors

--- tweets/views.py
+++ tweets/views.py
@@ -14,7 +14,6 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_create_view(request, *args, **kwargs):
-    print(abc)
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
     if form.is_valid():
