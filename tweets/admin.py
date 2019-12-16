e4f6e285b029722925df1d58265a127d10224214
CFE <hello@teamcfe.com>
2019-12-16 15:31:06 -0800

    34 - Django Admin

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tweet
+
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'user']
+    search_fields = ['content', 'user__username', 'user__email']
+    class Meta:
+        model = Tweet
+
+admin.site.register(Tweet, TweetAdmin)
+
+
--- tweets/models.py
+++ tweets/models.py
@@ -10,7 +10,10 @@ class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) # many users can many tweets
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
-
+    
+    # def __str__(self):
+    #     return self.content
+    
     class Meta:
         ordering = ['-id']
     
