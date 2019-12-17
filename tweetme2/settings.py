f0b6d26180c5a8c86e2e30df7a2c0f3a585c4472
CFE <hello@teamcfe.com>
2019-12-16 16:10:43 -0800

    37 - Install Django Rest Framework

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # third-party
+    'rest_framework',
+    # internal
     'tweets',
 ]
 
