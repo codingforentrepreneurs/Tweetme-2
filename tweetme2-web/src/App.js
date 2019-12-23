28584e8be0455a8e6bb36a8236046843add92479
CFE <hello@teamcfe.com>
2019-12-23 15:29:04 -0800

    55 - Handling CORS and Invalid HOST_HEADER in Django

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function loadTweets(callback) {
   const xhr = new XMLHttpRequest()
   const method = 'GET' // "POST"
-  const url = "http://127.0.0.1:8000/api/tweets/"
+  const url = "http://localhost:8000/api/tweets/"
   const responseType = "json"
   xhr.responseType = responseType
   xhr.open(method, url)
--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '#)=h)_wc*k%f=wk+!$x0t%1wx7*_50$a1%*75s$og(8$27$ju1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.cfe.sh']
+ALLOWED_HOSTS = ['127.0.0.1', '.cfe.sh', 'localhost']
 LOGIN_URL = "/login"
 
 MAX_TWEET_LENGTH = 240
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third-party
+    'corsheaders',
     'rest_framework',
     # internal
     'tweets',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CORS_ORIGIN_ALLOW_ALL = True # any website has access to my api
+CORS_URLS_REGEX = r'^/api/.*$'
 
 
 DEFAULT_RENDERER_CLASSES = [
