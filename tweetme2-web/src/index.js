168382442ca1efeb90067c9c2ebd92a90027bb90
CFE <hello@teamcfe.com>
2019-12-23 20:59:04 -0800

    63 - Adjust the React Render Process

--- tweetme2-web/src/index.js
+++ tweetme2-web/src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {TweetsComponent} from './tweets'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const appEl = document.getElementById('root')
+if (appEl) {
+    ReactDOM.render(<App />, appEl);
+}
+const tweetsEl = document.getElementById("tweetme-2")
+if (tweetsEl) {
+    ReactDOM.render(<TweetsComponent />, tweetsEl);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
