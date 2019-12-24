2763cd207d10ba25737bc9c5fd45f2a76b332e4e
CFE <hello@teamcfe.com>
2019-12-23 21:25:10 -0800

    65 - Render React App via Any Django Template

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -31,7 +31,7 @@ from tweets.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
-    path('react/', TemplateView.as_view(template_name='react.html')),
+    path('react/', TemplateView.as_view(template_name='react_via_dj.html')),
     path('create-tweet', tweet_create_view),
     path('tweets', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
