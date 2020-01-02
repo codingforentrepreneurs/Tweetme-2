944cbcb5ad4c535eacf51aa6645f816e72f30edc
CFE <hello@teamcfe.com>
2020-01-02 11:11:13 -0800

    69 - Dev Authentication

---
+++ tweetme2/rest_api/dev.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import get_user_model
+from rest_framework import authentication
+
+
+User = get_user_model()
+
+class DevAuthentication(authentication.BasicAuthentication):
+    def authenticate(self, request):
+        qs = User.objects.all()
+        user = qs.order_by("?").first()
+        return (user, None)
--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -143,14 +143,18 @@ DEFAULT_RENDERER_CLASSES = [
         'rest_framework.renderers.JSONRenderer',
     ]
 
+DEFAULT_AUTHENTICATION_CLASSES = [
+    'rest_framework.authentication.SessionAuthentication'
+]
 if DEBUG:
     DEFAULT_RENDERER_CLASSES += [
         'rest_framework.renderers.BrowsableAPIRenderer',
     ]
+    DEFAULT_AUTHENTICATION_CLASSES += [
+        'tweetme2.rest_api.dev.DevAuthentication'
+    ]
 REST_FRAMEWORK = {
     
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication'
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': DEFAULT_AUTHENTICATION_CLASSES,
     'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES
 }
