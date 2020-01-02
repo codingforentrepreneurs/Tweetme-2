6dfdf9913f7389235bc0ca81c0deec6f90c76f04
CFE <hello@teamcfe.com>
2020-01-02 10:26:40 -0800

    67 - A Better XHR Lookup

--- tweetme2-web/src/lookup/components.js
+++ tweetme2-web/src/lookup/components.js
@@ -1,16 +1,21 @@
-export function loadTweets(callback) {
-    const xhr = new XMLHttpRequest()
-    const method = 'GET' // "POST"
-    const url = "http://localhost:8000/api/tweets/"
-    const responseType = "json"
-    xhr.responseType = responseType
-    xhr.open(method, url)
-    xhr.onload = function() {
-      callback(xhr.response, xhr.status)
-    }
-    xhr.onerror = function (e) {
-      console.log(e)
-      callback({"message": "The request was an error"}, 400)
-    }
-    xhr.send()
+function lookup(method, endpoint, callback, data) {
+  let jsonData;
+  if (data){
+    jsonData = JSON.stringify(data)
   }
+  const xhr = new XMLHttpRequest()
+  const url = `http://localhost:8000/api${endpoint}`
+  xhr.responseType = "json"
+  xhr.open(method, url)
+  xhr.onload = function() {
+    callback(xhr.response, xhr.status)
+  }
+  xhr.onerror = function (e) {
+    console.log(e)
+    callback({"message": "The request was an error"}, 400)
+  }
+  xhr.send(jsonData)
+}
+export function loadTweets(callback) {
+    lookup("GET", "/tweets/", callback)
+}
--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -34,6 +34,7 @@ export function TweetsComponent(props) {
 export function TweetsList(props) {
     const [tweetsInit, setTweetsInit] = useState([])
     const [tweets, setTweets] = useState([])
+    const [tweetsDidSet, setTweetsDidSet] = useState(false)
     useEffect(()=>{
       const final = [...props.newTweets].concat(tweetsInit)
       if (final.length !== tweets.length) {
@@ -42,15 +43,18 @@ export function TweetsList(props) {
     }, [props.newTweets, tweets, tweetsInit])
 
     useEffect(() => {
-      const myCallback = (response, status) => {
-        if (status === 200){
-          setTweetsInit(response)
-        } else {
-          alert("There was an error")
+      if (tweetsDidSet === false){
+        const myCallback = (response, status) => {
+          if (status === 200){
+            setTweetsInit(response)
+            setTweetsDidSet(true)
+          } else {
+            alert("There was an error")
+          }
         }
+        loadTweets(myCallback)
       }
-      loadTweets(myCallback)
-    }, [tweetsInit])
+    }, [tweetsInit, tweetsDidSet, setTweetsDidSet])
     return tweets.map((item, index)=>{
       return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`} />
     })
