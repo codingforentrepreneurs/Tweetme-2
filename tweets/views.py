c5306bb9cacac68268a1ac5bd4eb28f5610e2767
CFE <hello@teamcfe.com>
2020-01-03 11:32:01 -0800

    76 - Limit List View by Username

--- tweets/views.py
+++ tweets/views.py
@@ -97,8 +97,11 @@ def tweet_action_view(request, *args, **kwargs):
 @api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
+    username = request.GET.get('username') # ?username=Justin
+    if username != None:
+        qs = qs.filter(user__username__iexact=username)
     serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data, status=200)
+    return Response( serializer.data, status=200)
 
 
 
