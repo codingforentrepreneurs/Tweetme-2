55d983e16241705e01128164d351249cfb4267c8
CFE <hello@teamcfe.com>
2020-01-03 15:32:04 -0800

    80 - Linking Individual Tweets

--- tweetme2-web/src/tweets/detail.js
+++ tweetme2-web/src/tweets/detail.js
@@ -16,7 +16,15 @@ export function ParentTweet(props){
       const {tweet, didRetweet, hideActions} = props
       const [actionTweet, setActionTweet] = useState(props.tweet ? props.tweet : null)
       const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+      const path = window.location.pathname
+      const match = path.match(/(?<tweetid>\d+)/)
+      const urlTweetId = match ? match.groups.tweetid : -1
+      const isDetail = `${tweet.id}` === `${urlTweetId}`
       
+      const handleLink = (event) => {
+        event.preventDefault()
+        window.location.href = `/${tweet.id}`
+      }
       const handlePerformAction = (newActionTweet, status) => {
         if (status === 200){
           setActionTweet(newActionTweet)
@@ -33,12 +41,16 @@ export function ParentTweet(props){
                 <p>{tweet.id} - {tweet.content}</p>
                 <ParentTweet tweet={tweet} />
               </div>
-          {(actionTweet && hideActions !== true) && <div className='btn btn-group'>
+          <div className='btn btn-group'>
+          {(actionTweet && hideActions !== true) && <React.Fragment>
                   <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "like", display:"Likes"}}/>
                   <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "unlike", display:"Unlike"}}/>
                   <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "retweet", display:"Retweet"}}/>
+                </React.Fragment>
+          }
+                  {isDetail === true ? null : <button className='btn btn-outline-primary btn-sm' onClick={handleLink}>View</button>}
                 </div>
-        }
+       
       </div>
     }
   
