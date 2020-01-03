82eb8cf2e88e5a1aa99947103a5a10f666a36c2e
CFE <hello@teamcfe.com>
2020-01-03 11:15:00 -0800

    75 - Set Data Props on ReactDOM Render

--- tweetme2-web/src/index.js
+++ tweetme2-web/src/index.js
@@ -9,9 +9,11 @@ const appEl = document.getElementById('root')
 if (appEl) {
     ReactDOM.render(<App />, appEl);
 }
+const e = React.createElement
 const tweetsEl = document.getElementById("tweetme-2")
 if (tweetsEl) {
-    ReactDOM.render(<TweetsComponent />, tweetsEl);
+    ReactDOM.render(
+        e(TweetsComponent, tweetsEl.dataset), tweetsEl);
 }
 
 // If you want your app to work offline and load faster, you can change
