e4761aa629c02c665be78b6f82c9fb26a4219d7a
CFE <hello@teamcfe.com>
2020-01-03 16:55:12 -0800

    83 - Login Required Redirect

--- tweetme2-web/src/lookup/components.js
+++ tweetme2-web/src/lookup/components.js
@@ -27,16 +27,21 @@ export function backendLookup(method, endpoint, callback, data) {
   xhr.setRequestHeader("Content-Type", "application/json")
 
   if (csrftoken){
-    xhr.setRequestHeader("HTTP_X_REQUESTED_WITH", "XMLHttpRequest")
+    // xhr.setRequestHeader("HTTP_X_REQUESTED_WITH", "XMLHttpRequest")
     xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest")
     xhr.setRequestHeader("X-CSRFToken", csrftoken)
   }
   
   xhr.onload = function() {
+    if (xhr.status === 403) {
+      const detail = xhr.response.detail
+      if (detail === "Authentication credentials were not provided."){
+        window.location.href = "/login?showLoginRequired=true"
+      }
+    }
     callback(xhr.response, xhr.status)
   }
   xhr.onerror = function (e) {
-    console.log(e)
     callback({"message": "The request was an error"}, 400)
   }
   xhr.send(jsonData)
