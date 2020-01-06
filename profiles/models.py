b331593f11aa5d792f5cc1a4916bb829a12196b4
CFE <hello@teamcfe.com>
2020-01-06 15:47:52 -0800

    87 - Signals to Create Profile Objects

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
 
 User = settings.AUTH_USER_MODEL
 
@@ -7,3 +8,11 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=220, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
+
+def user_did_save(sender, instance, created, *args, **kwargs):
+    if created:
+        Profile.objects.get_or_create(user=instance)
+
+post_save.connect(user_did_save, sender=User)
+
+# after the user logs in -> verify profile
