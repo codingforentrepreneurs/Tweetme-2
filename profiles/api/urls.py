72db184c5a6b9338eecba1c2df630812fa269faa
CFE <hello@teamcfe.com>
2020-01-20 16:49:32 -0800

    103 - User Profile API Detail

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from .views import (
     user_follow_view,
+    profile_detail_api_view,
 )
 '''
 CLIENT
 Base ENDPOINT /api/profiles/
 '''
 urlpatterns = [
+    path('<str:username>/', profile_detail_api_view),
     path('<str:username>/follow', user_follow_view),
 ]
--- profiles/api/views.py
+++ profiles/api/views.py
@@ -11,6 +11,7 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from ..models import Profile
+from ..serializers import PublicProfileSerializer
 
 User = get_user_model()
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
@@ -21,6 +22,16 @@ ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 #     current_user = request.user
 #     to_follow_user = ??
 #     return Response({}, status=200)
+
+@api_view(['GET'])
+def profile_detail_api_view(request, username, *args, **kwargs):
+    # get the profile for the passed username
+    qs = Profile.objects.filter(user__username=username)
+    if not qs.exists():
+        return Response({"detail": "User not found"}, status=404)
+    profile_obj = qs.first()
+    data = PublicProfileSerializer(instance=profile_obj)
+    return Response(data.data, status=200)
 
 
 @api_view(['GET', 'POST'])
